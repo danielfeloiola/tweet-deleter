ffb71bc2281942a1daabdd6f60a8420046f6b3fa
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2020-05-04 18:02:08 -0300

    bugfix

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Um simples script que deleta tweets
-A simple script to delete tweets
--- delete.py
+++ delete.py
@@ -9,7 +9,7 @@ import os
 
 # pegas as chaves
 consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
