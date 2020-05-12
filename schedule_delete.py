58e9fd28cff018a6d26e76ef1022baed5e270a71
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2020-05-12 14:42:28 -0300

    mudança no agendamento

--- schedule_delete.py
+++ schedule_delete.py
@@ -40,7 +40,7 @@ def delete():
         api.destroy_status(tweet.id)
 
 # repete a tarefa diariamente as 14:15
-schedule.every().day.at("14:35").do(delete)
+schedule.every().day.at("14:45").do(delete)
 
 # roda permanentemente
 while True:
